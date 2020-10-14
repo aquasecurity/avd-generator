6f01343fd7bbe042decc9a7f5238b087f0b12ce1
Simarpreet Singh <simar@linux.com>
2020-10-13 18:23:44 -0700

    avd: Automate Kube Hunter page generation logic.
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- main.go
+++ main.go
@@ -421,6 +421,7 @@ func ParseRegoPolicyFile(fileName string) (rp RegoPost, err error) {
 func main() {
 	generateVulnPages()
 	generateRegoPages()
+	generateKubeHunterPages("kube-hunter-repo/docs/_kb", "content/kube-hunter")
 }
 
 func generateVulnPages() {
@@ -457,6 +458,37 @@ func generateRegoPages() {
 		policyDir := filepath.Join("appshield-repo", "policies", p, "policy")
 		log.Printf("generating policies in: %s...", policyDir)
 		generateRegoPolicyPages(policyDir, "content/appshield")
+	}
+}
+
+func generateKubeHunterPages(inputPagesDir string, outputPagesDir string) {
+	log.Printf("generating kube-hunter pages in: %s...", outputPagesDir)
+	pages, err := GetAllFiles(inputPagesDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, page := range pages {
+		b, err := ioutil.ReadFile(filepath.Join(inputPagesDir, page))
+		if err != nil {
+			log.Println("unable to read original kube hunter doc: ", err)
+			continue
+		}
+
+		newContent := strings.Replace(string(b), "---", `---
+avd_page_type: kube-hunter_page
+`, 1)
+		r := strings.NewReplacer(
+			"# {{ page.vid }} - {{ page.title }}", "",
+			"title", "description",
+			"vid", "title",
+			"categories: ", "types: ")
+		content := r.Replace(newContent)
+
+		err = ioutil.WriteFile(filepath.Join(outputPagesDir, page), []byte(content), 0644)
+		if err != nil {
+			log.Fatalln("unable to write kube hunter page: ", err)
+		}
 	}
 }
 
--- main_test.go
+++ main_test.go
@@ -766,3 +766,17 @@ func TestGenerateRegoPolicyPages(t *testing.T) {
 		}
 	})
 }
+
+func TestKubeHunterPages(t *testing.T) {
+	pagesDir, _ := ioutil.TempDir("", "TestKubeHunterPages-*")
+	defer func() {
+		_ = os.RemoveAll(pagesDir)
+	}()
+
+	generateKubeHunterPages("goldens/kube-hunter", pagesDir)
+	got, err := ioutil.ReadFile(filepath.Join(pagesDir, "KHV002-orig.md"))
+	require.NoError(t, err)
+
+	want, _ := ioutil.ReadFile("goldens/kube-hunter/KHV002-avd.md")
+	assert.Equal(t, string(want), string(got))
+}
