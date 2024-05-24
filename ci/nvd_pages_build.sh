b041ebe080fa2a714e32e6eedfac1a979edbee7b
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2024-05-24 13:30:26 +0600

    fix(ci): merge `searchindex.json` after each `hugo` run  (#92)

--- ci/nvd_pages_build.sh
+++ ci/nvd_pages_build.sh
@@ -3,6 +3,10 @@ set -u
 
 CONTENT_NVD_DIR="content-nvd"
 
+## hugo overwrites searchindex.json files after each run.
+## Create all-searchindex.json to combine all indexes from searchindex.json files into this file.
+touch all-searchindex.json
+
 for file in "$CONTENT_NVD_DIR"/*
 do
   YEAR="${file##*/}"
@@ -12,9 +16,14 @@ do
   else
     printf "\n===Building NVD $YEAR pages===\n"
     hugo --destination=docs -c "$CONTENT_NVD_DIR/$YEAR" ## build nvd pages by year
+    ## merge all nvd indexes to all-searchindex.json file
+    jq -sc '.[0] + .[1]' all-searchindex.json docs/searchindex.json > all-searchindex.tmp && mv all-searchindex.tmp all-searchindex.json
   fi
 done
 
 ## build `compliance`, `misconfig`, `tracee` and `nvd/index.md` pages
 printf "\n===Building the remaining content===\n"
 hugo --destination=docs
+
+## merge nvd and other indexes to docs/searchindex.json
+jq -sc '.[0] + .[1]' all-searchindex.json docs/searchindex.json > all-searchindex.tmp && mv all-searchindex.tmp docs/searchindex.json
